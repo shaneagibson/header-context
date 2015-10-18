bed6bc01c9fa173b488e5eccaeb5e36e6bcf6475
Shane Gibson <shane.gibson@digital.hmrc.gov.uk>
2015-10-18 08:46:05 +0100

    added test for parallel execution

--- src/test/scala/uk/co/epsilontechnologies/headercarrier/HeaderContextSpec.scala
+++ src/test/scala/uk/co/epsilontechnologies/headercarrier/HeaderContextSpec.scala
@@ -39,11 +39,25 @@ class HeaderContextSpec extends WordSpecLike with Matchers with HeaderContext wi
       }
     }
 
+    def parallel() = Action.async {
+      implicit request => {
+        val id = request.getQueryString("id").getOrElse("")
+        captureHeaders(request.headers)
+        Future.successful {
+          retrieveHeaders().getOrElse("test", "").equals(s"parallel-$id") match {
+            case true => Ok("")
+            case false => BadRequest(s"test header should be 'parallel-$id'")
+          }
+        }
+      }
+    }
+
     val application = FakeApplication(
       additionalConfiguration = Map("play.akka.actor.default-dispatcher.type" -> "uk.co.epsilontechnologies.headercontext.ContextPropagatingDispatcherConfigurator"),
       withRoutes = {
         case ("GET", "/same-thread") => sameThread()
         case ("GET", "/different-thread") => differentThread()
+        case ("GET", "/parallel") => parallel()
       }
     )
 
@@ -57,7 +71,12 @@ class HeaderContextSpec extends WordSpecLike with Matchers with HeaderContext wi
       await(WS.url("http://localhost:9000/different-thread").withHeaders(("test", "different-thread")).get()).status shouldBe 200
     }
 
-    // TODO - add test for simultaneous requests
+    "capture and fetch header values when multiple requests are issued in parallel" in running(server) {
+      val futures = (1 to 1000).map(i => {
+        WS.url(s"http://localhost:9000/parallel?id=$i").withHeaders(("test", s"parallel-$i")).get()
+      })
+      await(Future.sequence(futures)).foreach(result => result.status shouldBe 200)
+    }
 
   }
 
